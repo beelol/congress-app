20f1be1fdb1751b8c7d2bbe3cb508070213a40ae
Bilal Itani <bilalitani1@gmail.com>
2020-01-02 12:07:08 -0500

    Remove unused assets leftover from create-react-app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import Results from "./Results.jsx";
 import * as serviceWorker from "./serviceWorker";
 
