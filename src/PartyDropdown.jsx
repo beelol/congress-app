3643ef2a234b1487928b71b215c25f33a1746b3b
Bilal Itani <bilalitani1@gmail.com>
2020-01-03 00:21:38 -0500

    Add an All selection to party and state filters to disable filters

--- src/PartyDropdown.jsx
+++ src/PartyDropdown.jsx
@@ -4,6 +4,7 @@ import { states } from "./states";
 export default function PartyDropdown(props) {
   return (
     <select onChange={props.onChange}>
+      <option value={""}>All</option>
       <option value={"Republican"}>Republican</option>
       <option value={"Democrat"}>Democrat</option>
       <option value={"Independent"}>Independent</option>
--- src/StatesDropdown.jsx
+++ src/StatesDropdown.jsx
@@ -4,6 +4,7 @@ import { states } from "./states";
 export default function StatesDropdown(props) {
   return (
     <select onChange={props.onChange}>
+      <option value={""}>All</option>
       {Object.keys(states).map(symbol => (
         <option value={symbol}>{states[symbol]}</option>
       ))}
